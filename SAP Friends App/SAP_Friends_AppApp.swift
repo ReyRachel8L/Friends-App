81ce13e6c909f65aa1a701b388575a18856897d5
ReyRachel8L <rlim.rachel08@gmail.com>
2022-07-09 10:45:46 +0800

    Initial Commit

---
+++ SAP Friends App/SAP_Friends_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_Friends_AppApp.swift
+//  SAP Friends App
+//
+//  Created by LIM YEE SIN RACHEL stu on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_Friends_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
